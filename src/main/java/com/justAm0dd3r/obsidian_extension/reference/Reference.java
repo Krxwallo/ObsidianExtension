df52d6d65b5d7a8d267ddd7c628cc5117349a2fc
Krxwallo <a@b.c>
2020-07-13 12:08:28 +0200

    Added Reference class

---
+++ src/main/java/com/justAm0dd3r/obsidian_extension/reference/Reference.java
@@ -0,0 +1,9 @@
+package com.justAm0dd3r.obsidian_extension.reference;
+
+public class Reference {
+    public static final String MOD_ID = "obsidian_extension";
+    public static final String MOD_NAME = "Obsidian Extension";
+    public static final String AUTHOR = "justAm0dd3r";
+    public static final String VERSION = "1.0.0";
+
+}
