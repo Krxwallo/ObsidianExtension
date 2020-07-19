dc08c845dc79693954035c4157c0036daf255132
Krxwallo <a@b.c>
2020-07-19 13:46:40 +0200

    Version update (build.gradle and Reference class) (1.3.0)

--- src/main/java/com/justAm0dd3r/obsidian_extension/reference/Reference.java
+++ src/main/java/com/justAm0dd3r/obsidian_extension/reference/Reference.java
@@ -7,5 +7,5 @@ public class Reference {
     public static final String MOD_ID = "obsidian_extension";
     public static final String MOD_NAME = "Obsidian Extension";
     public static final String AUTHOR = "justAm0dd3r";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 }
